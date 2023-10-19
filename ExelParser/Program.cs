f6d11e93ef2efc4ac1697b300a51657cd824ca06
Andrey Batukhtin <batyhtik3@gmail.com>
2023-10-19 23:34:06 +0300

    same fixs

--- ExelParser/Program.cs
+++ ExelParser/Program.cs
@@ -10,10 +10,12 @@ namespace ExelParser
 {
     class Program
     {
-        private static string file = "Test.xlsx";
+        private static readonly string _file = "Test.xlsx";
+        private static readonly int[] _requiredColumns = { 4, 10, 18, 19, 26, 27, 28, 30, 32, 44, 49, 51, 151 };
+        private static readonly int _numberRowsForProgress = 100;
         static void Main(string[] args)
         {
-            Console.WriteLine($"Работа с файлом {file}");
+            Console.WriteLine($"Работа с файлом {_file}");
             Console.WriteLine("Чтобы запустить работу нажмити \"Enter\"");
             Console.ReadLine();
 
@@ -22,11 +24,10 @@ namespace ExelParser
         private static void StartParser()
         {
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"Работа с файлом {file} началась!");
+            Console.WriteLine($"Работа с файлом {_file} началась!");
             Console.ResetColor();
 
             Excel.Sheets _sheets;
-            int[] requiredColumns = { 4, 10, 18, 19, 26, 27, 28, 30, 32, 44, 49, 51, 151 };
             try
             {
                 using (ExcelHelper helper = new ExcelHelper())
@@ -36,53 +37,44 @@ namespace ExelParser
                         _sheets = helper._workbook.Sheets;
 
                         foreach (Excel.Worksheet worksheet in _sheets)
-                        {
-                            // Получаем диапазон используемых на странице ячеек
-                            Excel.Range UsedRange = worksheet.UsedRange;
-                            // Получаем строки в используемом диапазоне
-                            Excel.Range urRows = UsedRange.Rows;
-                            // Получаем столбцы в используемом диапазоне
-                            Excel.Range urColums = UsedRange.Columns;
+                        {  
+                            Excel.Range UsedRange = worksheet.UsedRange; // Получаем диапазон используемых на странице ячеек        
+                            Excel.Range urRows = UsedRange.Rows; // Получаем строки в используемом диапазоне
+                            Excel.Range urColums = UsedRange.Columns; // Получаем столбцы в используемом диапазоне
 
-                            // Количества строк и столбцов
                             int RowsCount = urRows.Count;
                             int ColumnsCount = urColums.Count;
                             for (int i = 1; i <= RowsCount; i++)
                             {
-                                foreach (var column in requiredColumns)
+                                foreach (var column in _requiredColumns)
                                 {
                                     Excel.Range CellRange = UsedRange.Cells[i, column];
-                                    // Получение текста ячейки
-                                    string CellText = (CellRange == null || CellRange.Value2 == null) ? null :
+
+                                    string cellText = (CellRange == null || CellRange.Value2 == null) ? null :
                                                         (CellRange as Excel.Range).Value2.ToString();
 
-                                    if (CellText != null)
+                                    if (cellText != null)
                                     {
-                                        //UsedRange.Cells[1, 1] = "qqq";
-                                        helper.Set(11, 2, data: "MYTEST2");
+                                        //helper.Set(11, 2, data: "MYTEST2");
                                     }
                                 }
-                                if (i % 2 == 0)
+                                if (i % _numberRowsForProgress == 0)
                                 {
-                                    //Console.Clear();
+                                    Console.Clear();
                                     Console.WriteLine($"Прогресс: {i}/{RowsCount}");
                                 }
                             }
+                            #region 
                             // Очистка неуправляемых ресурсов на каждой итерации
                             //if (urRows != null) Marshal.ReleaseComObject(urRows);
                             //if (urColums != null) Marshal.ReleaseComObject(urColums);
                             //if (UsedRange != null) Marshal.ReleaseComObject(UsedRange);
                             //if (worksheet != null) Marshal.ReleaseComObject(worksheet);
+                            #endregion
                         }
-                        helper.Set(column: 5, row: 1, data: "lksadklsajdkl");
-                        //var val = helper.Get(column: "A", row: 6);
-                        helper.Set(column: 5, row: 1, data: DateTime.Now);
-
                         helper.Save();
                     }
                 }
-
-                Console.Read();
             }
             catch (Exception ex) { Console.WriteLine(ex.Message); }
         }
