f39633e8f60d3909504d51bebfa2253fbdda7bb7
Andrey Batukhtin <batyhtik3@gmail.com>
2023-10-19 19:26:51 +0300

    Add project files.

---
+++ ExelParser/BO/ExcelHelper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExelParser.BO
+{
+    class ExcelHelper : IDisposable
+    {
+        private Excel.Application _excel;
+        private Excel.Workbook _workbook;
+        private string _filePath;
+
+        public ExcelHelper()
+        {
+            _excel = new Excel.Application();
+        }
+
+        internal bool Open(string filePath)
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    _workbook = _excel.Workbooks.Open(filePath);
+                    _filePath = filePath;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Ошибка при открытии! Файла {filePath} не существует!");
+                    Console.ResetColor();
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            return false;
+        }
+
+        internal void Save()
+        {
+            if (string.IsNullOrEmpty(_filePath))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Ошибка при сохранении! Файла {_filePath} не существует!");
+                Console.ResetColor();
+                return;
+            }
+            else
+            {
+                _workbook.Save();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Файл {_filePath} успешно сохранен!");
+                Console.ResetColor();
+            }
+        }
+
+        internal bool Set(string column, int row, object data)
+        {
+            try
+            {
+                ((Excel.Worksheet)_excel.ActiveSheet).Cells[row, column] = data;
+                return true;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            return false;
+        }
+
+        internal object Get(string column, int row)
+        {
+            try
+            {
+                return ((Excel.Worksheet)_excel.ActiveSheet).Cells[row, column].Value2;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            return null;
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                _workbook.Close();
+                _excel.Quit();
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
---
+++ ExelParser/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExelParser
+{
+    internal class Program
+    {
+        private static string file = "Test.xlsx";
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Работа с файлом {file}");
+            Console.WriteLine("Чтобы запустить работу нажмити \"Enter\"");
+            Console.ReadLine();
+            StartSetValue();
+        }
+        private static void StartSetValue()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Работа с файлом {file} началась.");
+            Console.ResetColor();
+            try
+            {
+                using (BO.ExcelHelper helper = new BO.ExcelHelper())
+                {
+                    if (helper.Open(filePath: Path.Combine(Environment.CurrentDirectory, file)))
+                    {
+                        helper.Set(column: "A", row: 2, data: "Test");
+                        //var val = helper.Get(column: "A", row: 6);
+                        helper.Set(column: "B", row: 2, data: DateTime.Now);
+
+                        helper.Save();
+                    }
+                }
+
+                Console.Read();
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
