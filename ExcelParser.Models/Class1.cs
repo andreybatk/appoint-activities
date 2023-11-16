7361fde33d26d2e01e469761113b09f1268e0ac7
Andrey Batukhtin <batyhtik3@gmail.com>
2023-11-16 20:57:37 +0300

    add library

---
+++ ExcelParser.Models/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace ExcelParser.Models
+{
+    public static class Columns
+    {
+        public static List<string> RequiredColumns = new List<string>
+        {   "KATL",
+            "OZU",
+            "Gr_voz",
+            "NPDR"
+        };
+        public static List<string> ColumnsForSettings = new List<string>
+        {
+            "PRVB",
+            "MER1",
+            "MER2"
+        };
+    }
+}
